964b3e38a3d2e99481b811a955fb23d0d766981b
Shiv Yadav <shivyadav0014@gmail.com>
2024-06-01 18:28:35 +0400

    Adding two numbers

---
+++ Add two numbers/Sum.cpp
@@ -0,0 +1,16 @@
+// Program to Add two numbers
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1, num2, sum;
+
+    cout << "Enter the two numbers" << endl;
+    cin >> num1 >> num2;
+    sum = num1 + num2;
+
+    cout << "The sum of " << num1 << " and " << num2 << " is " << sum;
+    return 0;
+}
